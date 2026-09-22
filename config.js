a14c224f87cfed00a3ab22b135cb9e2e8d490f08
oxilnovo <46940376+oxilnovo@users.noreply.github.com>
2026-09-22 13:56:39 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Create a Google OAuth Web client ID and deploy Code.gs as a Google Apps Script Web App.
 window.POULETTOS_CONFIG = {
   GOOGLE_CLIENT_ID: '663179170370-bp361u1avupqp2ut4fqt1hlhbcikbal0.apps.googleusercontent.com',
-  API_URL: 'https://script.google.com/macros/s/AKfycbzgySToyXW1Q6l6bDbYT_JwwAWby9QFgd8cuqxcZRPVlUSMsLZIOsy4Vk5mr3RaMhgD/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbxKn605MuWzBcYi_90uDPNfZFfczW2N0MUIbYjEBYolEK3ZgxWQIm4TPjG4WNP2bwZ2/exec'
 };
