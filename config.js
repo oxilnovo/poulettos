acf31849e18939c243f8cda3399424d13fb9f3c2
oxilnovo <46940376+oxilnovo@users.noreply.github.com>
2026-09-22 13:33:35 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Poulettos V3 configuration.
+// Create a Google OAuth Web client ID and deploy Code.gs as a Google Apps Script Web App.
+window.POULETTOS_CONFIG = {
+  GOOGLE_CLIENT_ID: '663179170370-bp361u1avupqp2ut4fqt1hlhbcikbal0.apps.googleusercontent.com',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzMvcGSIRI0FBfJGuLZtqsgt9sXhAcvTKKYVkkIw890gc9Z618avTb26T9SBRh9tJqs/exec'
+};
